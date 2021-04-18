8f7eb29a24752ea3235ab3791df96ba2ba8e4797
markdflitter <markdflitter@googlemail.com>
2021-04-18 18:20:35 +0100

    jump and go are now the same and don't set A6

--- convert.bat
+++ convert.bat
@@ -1,2 +1,3 @@
-"..\..\..\..\68000 utils\srec\srec_cat" MDFmon.s68 -split 2 0 -o MDFmon.bin.high -binary
-"..\..\..\..\68000 utils\srec\srec_cat" MDFmon.s68 -split 2 1 -o MDFmon.bin.low -binary
+"..\..\68000 utils\srec\srec_cat" MDFmon.s68 -split 2 0 -o MDFmon.bin.high -binary
+"..\..\68000 utils\srec\srec_cat" MDFmon.s68 -split 2 1 -o MDFmon.bin.low -binary
+pause
